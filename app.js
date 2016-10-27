bbd69059c531526f81e2d217689a7138556bb4a0
AoranZhao <aoran_zhao@yahoo.ca>
2016-10-27 02:15:47 -0400

    update port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 // var db = mongoose.connection;
 
 app = configure(app);
-app.set('port', process.env.PORT || 3500);
+app.set('port', process.env.PORT || 80);
 
 app.listen(app.get('port'), function() {
 	console.log('server starting at port', app.get('port'));
